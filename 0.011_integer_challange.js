5655489542f2b2391f4153cb5015074732b4f8a0
Brett Mullenix <brettmullenix@gmail.com>
2017-06-21 10:26:07 -0400

    JavaScript Library
    blah

---
+++ 0.011_integer_challange.js
@@ -0,0 +1,22 @@
+//Bronze: Display four variables
+var age = 20;
+var birthyear = 1996;
+var gradyear = 2015;
+var numsiblins = 5;
+console.log(age);
+console.log(birthyear);
+console.log(gradyear);
+console.log(numsiblins);
+
+//Silver: Make four prompts, one equaling 1, then 2, then 3, then 12
+var x = 10;
+var y = 4;
+
+console.log(x % y); 
+console.log(x-9);
+console.log((y*1) - 2);
+console.log(36/3);
+
+//Gold Display the quoted text "Is it true that Gummy Worms is not a number? -> true/false"
+var isNaN = "Gummy Worms"
+console.log("Is it true that " + isNaN + " is not a number? -> " + (isNaN != 1));
---
+++ 0.021_variable_challange.js
@@ -0,0 +1,29 @@
+//Bronze
+var x = 1;
+var y = 2;
+var z = 3;
+var q = 4;
+var r = 5;
+
+console.log("My bank account has this many dollars in it: $" + (x +y + z));
+console.log("The number of car crashes today are: " + q);
+console.log("My nephew turns " + r + " today");
+
+//Silver
+var myNumber = 0;
+myNumber += 10;
+console.log(myNumber);
+myNumber -= 1;
+console.log(myNumber);
+myNumber *= 9;
+console.log(myNumber);
+myNumber /= 7;
+console.log(myNumber);
+
+//Gold
+var age = 20;
+var maxage = 100;
+var storingrate = 10;
+var x = storingrate * 365 * (maxage - 20);//10 a day for 365 days a year for 80 years (100-20).
+
+console.log("You will need " + x + " bottles of mountain dew to last you until the ripe age of " + maxage);
